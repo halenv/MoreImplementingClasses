fc700a8bc0af72cbdd22f5ac344bd8bad185e3f3
halenv <halenv@rose-hulman.edu>
2018-03-26 09:39:09 -0400

    Module 2 finished

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,8 +2,8 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Noelle Hale.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -28,6 +28,7 @@ def main():
         Baby McKinley is CRYING uncontrollably!  Feed the Baby!
         Baby McKinley is CRYING uncontrollably!  Feed the Baby!
     """
+
     mckinley = Baby('McKinley')
     keegan = Baby('Keegan')
     for k in range(2):
@@ -39,7 +40,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# DONE: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
@@ -90,10 +91,30 @@ def main():
 #
 ########################################################################
 
+class Baby(object):
 
+    def __init__(self, name):
+        self.name = name
+        self.fed = 1
+        print('Hello baby ' + self.name + '!')
 
+    def __repr__(self):
+        return 'Hello baby ' + self.name + '!'
 
+    def feed_baby(self):
+        print('Thank you for feeding baby ' + self.name + '!')
+        self.fed = 1
 
+    def hour_passes(self):
+
+        if self.fed == 1:
+            print('Baby ' + self.name + ' is sleeping.')
+        elif self.fed == 0:
+            print('Baby ' + self.name + ' is awake. Time for food.')
+        elif self.fed < 0:
+            print('Baby ' + self.name + ' is CRYING uncontrollably! Feed the Baby!')
+
+        self.fed = self.fed - 1
 
 
 # ----------------------------------------------------------------------
